a0568040682b2bf2d2c996676241c887dbc171e0
Khalid Chaudhry <kchaud@microsoft.com>
2019-12-01 13:02:13 -0800

    added stack project

---
+++ Concepts/Stack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SortedInsert si = new SortedInsert();
+            Stack<int> stk = new Stack<int>();
+            stk.Push(0);
+            stk.Push(3);
+            stk.Push(4);
+            stk.Push(7);
+
+            si.SortedInsertWithRecursion(stk,1);
+
+            foreach (var one in stk)
+            {
+                Console.WriteLine(one);
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}
---
+++ Concepts/Stack/SortedInsert.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack
+{
+    class SortedInsert
+    {
+
+        public Stack<int> SortedInsertWithoutRecursion(Stack<int> stk,int element)
+        {
+            if (element >= stk.Peek())
+            {
+                stk.Push(element);
+                return stk;            
+            }
+            Stack<int> stk2 = new Stack<int>();
+
+            while (stk.Count != 0)
+            {
+                stk2.Push(stk.Pop());
+            }
+
+            var isInserted = false;
+            while (stk2.Count != 0)
+            {
+                var pop = stk2.Pop();
+
+                if (element < pop && !isInserted)
+                {
+                    stk.Push(element);
+                    stk.Push(pop);
+                }
+                else
+                {
+                    stk.Push(pop);
+                }
+            }
+
+            return stk;
+        
+        }
+        public void SortedInsertWithRecursion(Stack<int> stk, int element)
+        {
+            if (stk.Count == 0 || element > stk.Peek())
+            {
+                stk.Push(element);
+            }
+            else
+            {
+                var temp = stk.Pop();
+                SortedInsertWithRecursion(stk, element);
+                stk.Push(temp);
+            
+            }
+
+        }
+    }
+}
