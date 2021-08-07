354e6f8413897a9979943dde9df59802d79fb81d
khalidchaudhry <khalidchaudhry2@hotmail.com>
2021-08-07 10:33:44 -0700

    Bits code

---
+++ Concepts/BitsFundas/NumberOdd.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitsFundas
+{
+    public static class NumberOdd
+    {
+        public static bool IsNumberOdd(int number)
+        {
+            return (number & 1) != 0 ? true : false;
+        }
+    }
+}
